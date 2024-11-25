914491228c9a4e1c5af793c4f48d5ee631da45c2
César Vaz <cesar_augusto_perfil@hotmail.com>
2024-11-25 13:49:51 -0300

    criação do menu

--- main.py
+++ main.py
@@ -8,3 +8,23 @@ def initialize_csv():
         with open(FILE_NAME, mode="w", newline="") as file:
             writer = csv.writer(file)
             writer.writerow(["ID", "Nome", "Telefone", "Email"])
+
+def main_menu():
+    while True:
+        print("\nAgenda de Contatos")
+        print("1. Criar Contato")
+        print("2. Listar Contatos")
+        print("3. Atualizar Contato")
+        print("4. Deletar Contato")
+        print("5. Sair")
+        choice = input("Escolha uma opção: ")
+
+        if choice == "1":
+        elif choice == "2":
+        elif choice == "3":
+        elif choice == "4":
+        elif choice == "5":
+            print("Saindo da Agenda. Até mais!")
+            break
+        else:
+            print("Opção inválida. Tente novamente.")
